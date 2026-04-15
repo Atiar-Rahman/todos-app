853b4edfa16423979675e723f00bd15b40d796ec
Atiar-Rahman <atiar1709@gmail.com>
2026-04-16 00:48:24 +0600

    ready for deploy

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4ti@inx@-1s%#wfu5s^yt4=%7_sd1@ximn!+!jpr@d(&btz1qp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
